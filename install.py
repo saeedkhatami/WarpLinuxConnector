246fd965397ef6a195a5c03863a5963bdcc926f0
0xb4dc0d3x <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>
2022-06-07 00:29:32 +0430

    wayland error on start fixed

--- install.py
+++ install.py
@@ -6,6 +6,7 @@ cur_path = sys.path[0]
 
 os.system("pip3 install -U pip wheel setuptools")
 os.system("pip3 install -r requirements.txt")
+os.system("sudo apt-get install libxcb-xinerama0")
 os.system("cp {}/icons/logo.png ~/.local/share/icons/warp_gui.png".format(cur_path))
 
 desktop_file = '{}/.local/share/applications/WARP-Linux.desktop'.format(Path.home())
